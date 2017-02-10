1a20002e3990ecb7ca67b2f13064b7a948fe1f57
Daniel Bevenius <daniel.bevenius@gmail.com>
2017-02-10 14:22:17 +0100

    clean up of Gruntfile
    This also required a modification to application.js so that it exposes
    the server that it listens to. This is so that it can be closed by grunt
    when running the stopServers task.
    
    JIRA: https://issues.jboss.org/browse/FH-3119

--- application.js
+++ application.js
@@ -23,9 +23,12 @@ app.use(mbaasExpress.errorHandler());
 
 var port = process.env.FH_PORT || process.env.OPENSHIFT_NODEJS_PORT || 8001;
 var host = process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
-app.listen(port, host, function() {
+var server = app.listen(port, host, function() {
   console.log("App started at: " + new Date() + " on port: " + port); 
 });
 
 
-module.exports.app = app
+module.exports = {
+  app: app,
+  server: server
+};
