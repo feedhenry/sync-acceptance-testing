20a4f459fc37cde2cf1527bbac91f94717010241
Aiden Keating <akeating@redhat.com>
2017-02-10 17:41:13 +0000

    FH-3118 Add afterAll test to syncSpec
    Currently four records are created in the MongoDB collections for
    the tests. These are now deleted when all tests end.

--- application.js
+++ application.js
@@ -1,9 +1,10 @@
-var mbaasApi = require('fh-mbaas-api');
-var express = require('express');
-var mbaasExpress = mbaasApi.mbaasExpress();
-var cors = require('cors');
+const async = require('async');
+const mbaasApi = require('fh-mbaas-api');
+const express = require('express');
+const mbaasExpress = mbaasApi.mbaasExpress();
+const cors = require('cors');
 
-var app = express();
+const app = express();
 
 // Enable CORS for all requests
 app.use(cors());
@@ -18,6 +19,8 @@ app.use(express.static(__dirname + '/public'));
 // Note: important that this is added just before your own Routes
 app.use(mbaasExpress.fhmiddleware());
 
+app.post('/dataset/:datasetId/reset', resetDataset);
+
 // Important that this is last!
 app.use(mbaasExpress.errorHandler());
 
@@ -27,6 +30,39 @@ var server = app.listen(port, host, function() {
   console.log("App started at: " + new Date() + " on port: " + port); 
 });
 
+/**
+ * Request handler to delete all collections in MongoDB for a provided dataset.
+ */
+function resetDataset(req, res) {
+  // Define titles for default, updates and collision collections in MongoDB.
+  const dataset = req.params.datasetId;
+  const updates = dataset + '-updates';
+  const collisions = dataset + '-collision';
+
+  // Delete all records in the MongoDB collections for the dataset.
+  async.forEach([dataset, updates, collisions], function(collection, cb) {
+    // If this fails we don't want the test to fail.
+    resetCollection(collection, cb);
+  }, function(err) {
+    if (err) {
+      return res.json({error: err }).status(500);
+    }
+    return res.json({message: 'Dataset ' + dataset + ' reset'}).status(200);
+  });
+};
+
+/**
+ * Remove all records for a particular collection in MongoDB.
+ * 
+ * @param {string} collection - Name of the MongoDB collection.
+ * @param {Function} cb - Callback function.
+ */
+function resetCollection(collection, cb) {
+  mbaasApi.db({
+    act: 'deleteall',
+    type: collection
+  }, cb);
+};
 
 module.exports = {
   app: app,
