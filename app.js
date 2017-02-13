25643094145bb101068c7e594993faa8f236ed3d
Daniel Bevenius <daniel.bevenius@gmail.com>
2017-02-13 12:08:14 +0100

    remove console.log statements

--- app.js
+++ app.js
@@ -1,7 +1,2 @@
+// eslint-disable-next-line
 var $fh = require('fh-js-sdk');
-
-/* eslint-disable */
-console.log('TEST', $fh.sync);
-console.log('window.location.href', window.location.href);
-var datasetId = 'myShoppingList';
-/* eslint-enable */
--- application.js
+++ application.js
@@ -27,8 +27,6 @@ app.use(mbaasExpress.errorHandler());
 var port = process.env.FH_PORT || process.env.OPENSHIFT_NODEJS_PORT || 8001;
 var host = process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
 var server = app.listen(port, host, function() {
-  // eslint-disable-next-line
-  console.log("App started at: " + new Date() + " on port: " + port);
 });
 
 /**
