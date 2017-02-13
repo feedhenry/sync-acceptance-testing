310e49933159db51e4ada11b332bf42715617ea5
Daniel Bevenius <daniel.bevenius@gmail.com>
2017-02-13 09:47:47 +0100

    fix eslint errors in app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var $fh = require('fh-js-sdk');
 
-
+/* eslint-disable */
 console.log('TEST', $fh.sync);
 console.log('window.location.href', window.location.href);
-
 var datasetId = 'myShoppingList';
+/* eslint-enable */
